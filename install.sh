1789a2ac7fdc6b1bb9ef7967a684369fef13bcf6
NatholDallas <natholdallas@gmail.com>
2024-05-21 21:06:38 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,30 @@ fi
 mkdir $share
 echo "Make directory $share"
 
-/bin/mv -f $bash $fastfetch $fish $hypr $nvim $starship $share
+# Backup
+
+if [[ -d $bash ]]; then
+  /bin/mv -f $bash $share
+fi
+
+if [[ -d $fastfetch ]]; then
+  /bin/mv -f $fastfetch $share
+fi
+
+if [[ -d $fish ]]; then
+  /bin/mv -f $fish $share
+fi
+
+if [[ -d $hypr ]]; then
+  /bin/mv -f $hypr $share
+fi
+
+if [[ -f $starship ]]; then
+  /bin/mv -f $starship $share
+fi
+
+# Copy
+
 /bin/cp -rf ./config/* $config
 
 echo "Move your origin files to $share"
