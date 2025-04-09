22d63b7eb09d10e7c616ba8e1f96fd9cfd1319ae
NatholDallas <natholdallas@gmail.com>
2025-04-09 19:12:41 +0800

    yazi modify

--- src/config/yazi/init.lua
+++ src/config/yazi/init.lua
@@ -1,19 +1,19 @@
-require("full-border"):setup()
-require("bookmarks"):setup({
-	last_directory = { enable = false, persist = false },
-	persist = "all",
-	desc_format = "full",
-	file_pick_mode = "hover",
-	notify = {
-		enable = false,
-		timeout = 1,
-		message = {
-			new = "New bookmark '<key>' -> '<folder>'",
-			delete = "Deleted bookmark in '<key>'",
-			delete_all = "Deleted all bookmarks",
-		},
-	},
-})
+-- require("full-border"):setup()
+-- require("bookmarks"):setup({
+-- 	last_directory = { enable = false, persist = false },
+-- 	persist = "all",
+-- 	desc_format = "full",
+-- 	file_pick_mode = "hover",
+-- 	notify = {
+-- 		enable = false,
+-- 		timeout = 1,
+-- 		message = {
+-- 			new = "New bookmark '<key>' -> '<folder>'",
+-- 			delete = "Deleted bookmark in '<key>'",
+-- 			delete_all = "Deleted all bookmarks",
+-- 		},
+-- 	},
+-- })
 
 function Linemode:size_and_mtime()
 	local size = self._file:size()
