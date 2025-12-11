620341454c15561493ce566053541d900005b121
NatholDallas <natholdallas@gmail.com>
2025-12-12 01:37:38 +0800

    commit

--- src/config/nvim/lua/plugins/astroui.lua
+++ src/config/nvim/lua/plugins/astroui.lua
@@ -35,16 +35,6 @@ return {
     },
   },
   {
-    "comfysage/gruvboxed",
-    opts = { contrast_dark = "hard" },
-  },
-  {
-    "sainnhe/sonokai",
-    init = function() -- init function runs before the plugin is loaded
-      vim.g.sonokai_style = "shusia"
-    end,
-  },
-  {
     "Mofiqul/vscode.nvim",
     opts = {
       transparent = false,
