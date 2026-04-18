a8ca591674737805e74f78bf786d6e4034a720ee
natholdallas <natholdallas@gmail.com>
2026-04-18 12:50:11 +0800

    commit

--- src/config/nvim/lua/plugins/individual.lua
+++ src/config/nvim/lua/plugins/individual.lua
@@ -82,4 +82,34 @@ return {
       opts.keymap["<S-Tab>"] = { "select_prev", "fallback" }
     end,
   },
+
+  {
+    "supermaven-inc/supermaven-nvim",
+    event = "InsertEnter",
+    cmd = { "SupermavenUseFree", "SupermavenUsePro" },
+    opts = {
+      keymaps = {
+        accept_suggestion = "<C-f>", -- handled by completion engine
+      },
+    },
+    specs = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          options = {
+            g = {
+              -- set the ai_accept function
+              ai_accept = function()
+                local suggestion = require "supermaven-nvim.completion_preview"
+                if suggestion.has_suggestion() then
+                  vim.schedule(function() suggestion.on_accept_suggestion() end)
+                  return true
+                end
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
 }
